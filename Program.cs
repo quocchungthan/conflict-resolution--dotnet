441957cf952615546cae2f9886dcd3cc0f8f7f70
cbt <quocchung.than@gmail.com>
2025-10-27 10:40:20 +0700

    add test data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿var dataLoader = new DataLoaderService();
 var data = await dataLoader.GetAllTextBlocks(Path.Combine(AppContext.BaseDirectory, "Data"));
-Console.WriteLine(data.Count());
+Console.WriteLine("---\nTotal test cases: {0}", data.Count());
 foreach (var item in data)
 {
-	Console.WriteLine(item);
+	Console.WriteLine("OriginalHtml={0}, Latest={0}, NewHtml={0}", item.OriginalHtmlText, item.LatestHtmlPersisted, item.NewHtmlText);
 }
